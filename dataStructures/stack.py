28b68fb5cd7747fb0efb86f7d71d37d50a34f2c3
Sai Panneerselvam <saigopini.panneerselvam@gmail.com>
2024-02-28 20:37:02 -0800

    stack.py

--- dataStructures/stack.py
+++ dataStructures/stack.py
@@ -1,6 +1,8 @@
 """
-Building a stack
+Building a stack using both list and linkedlist implementations
 """
+from node import Node
+
 class Stack():
     def __init__(self):
         self.stack = []
@@ -23,7 +25,45 @@ class Stack():
     
     def returnStack(self):
         return "".join(self.stack)
+
+class StackLinkedList():
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def traverseList(self):
+        output = ""
+        current = self.head 
+        while current:
+            output += str(current.dataval) + " ---> "
+            current = current.nextval
+        output += " ---> None "
     
+    def push(self, data): #inserts at back
+        if self.head == None:
+            self.head = Node(data, None)
+        else:
+            current = self.head
+            while current.nextval:
+                current = current.nextval
+            current.nextval = Node(data, None)
+        self.length += 1
+    
+    def pop(self): #remove from the back
+        if self.head == None:
+            return "Empty list"
+        else:
+            current = self.head
+            counter = 0
+            while counter < self.length - 1 and current:
+                current = current.nextval
+                counter += 1
+            current.nextval = None
+            self.length -= 1
+    
+    
+    
+
 
 def removeStars(word):
     stack = Stack()
